90168cca23bd97b244b0905c8719b84252ed23a7
Vladimir Kravchenko <33279970+Wowanderful@users.noreply.github.com>
2024-08-23 20:29:09 -0300

    Delete new.py

--- new.py
+++
@@ -1,4 +0,0 @@
-def number_system():
-    return [a for a in range(10) if 8 * a ** 1 + 8 * a **0  == ((3 * a ** 1) + (2 * a ** 0)) + ((2 * a ** 1) + (2 * a ** 0)) + ((1 * a ** 1) + (6 * a ** 0)) + ((1 * a ** 1) + (7 * a ** 0))]
-                                                                                                                                       
-print(number_system())
